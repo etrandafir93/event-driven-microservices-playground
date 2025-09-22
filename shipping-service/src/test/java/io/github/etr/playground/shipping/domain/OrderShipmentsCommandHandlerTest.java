b1368d43c7facf5aaf863e7eff563ee48019726a
etrandafir93 <emanueltrandafir1993@gmail.com>
2025-09-22 16:18:55 +0300

    nested tests

--- shipping-service/src/test/java/io/github/etr/playground/shipping/domain/OrderShipmentsCommandHandlerTest.java
+++ shipping-service/src/test/java/io/github/etr/playground/shipping/domain/OrderShipmentsCommandHandlerTest.java
@@ -2,12 +2,14 @@ package io.github.etr.playground.shipping.domain;
 
 import static org.assertj.core.api.Assumptions.assumeThat;
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
 import static org.awaitility.Awaitility.await;
 
 import java.util.Map;
 
-import org.assertj.core.api.Assumptions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -15,22 +17,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import io.github.etr.playground.IntegrationTest;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayName("Given 'stock-reserved' event received on Kafka")
 class OrderShipmentsCommandHandlerTest extends IntegrationTest {
 
     @Autowired
     private OrderShipmentsRepository shipmentsRepo;
 
-    @Autowired
-    private OrderShipmentsCommandHandler commandHandler;
-
-    @Autowired
-    private OrderShipmentsQueries orderShipmentsQueries;
-
     private static String trackingNumber;
 
     @Test
-    @Order(1)
+    @DisplayName("Then a new OrderShipment is saved to DB")
     void shouldCreateShipmentWhenStockReserved() {
         sendKafkaMessage("stock-reserved", "order-100", """
             {
@@ -54,57 +50,75 @@ class OrderShipmentsCommandHandlerTest extends IntegrationTest {
         trackingNumber = shipment.trackingNumber();
     }
 
-    @Test
-    @Order(2)
-    void shouldPublishOrderPacked() {
-        assumeThat(trackingNumber).isNotNull();
+    @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+    @DisplayName("When PUT /shipments/:id/pack is received over HTTP")
+    class PackOrder {
 
-        sendPutRequest("/shipments/%s/pack".formatted(trackingNumber), Map.of(
-            "packedAt", "2025-09-22T11:37:24.000Z",
-            "estimatedShippingDate", "2025-09-22T11:37:24.000Z"
-        ));
+        @Test
+        @Order(1)
+        @DisplayName("Then 'order-packed' is published to Kafka")
+        void shouldPublishOrderPacked() {
+            assumeThat(trackingNumber).isNotNull();
 
-        await().untilAsserted((() ->
-            then(outgoingKafkaMessages.messagesFor("order-packed"))
-                .hasSize(1).first()
-                .hasFieldOrPropertyWithValue("orderId", "order-100")
-                .hasFieldOrPropertyWithValue("username", "john_doe")
-                .hasFieldOrPropertyWithValue("trackingNumber", trackingNumber)));
+            sendPutRequest("/shipments/%s/pack".formatted(trackingNumber),
+                Map.of("packedAt", "2025-09-22T11:37:24.000Z", "estimatedShippingDate", "2025-09-22T11:37:24.000Z"));
+
+            await().untilAsserted((() -> then(outgoingKafkaMessages.messagesFor("order-packed"))
+                .hasSize(1).first().asInstanceOf(MAP)
+                .containsAllEntriesOf(Map.of(
+                    "orderId", "order-100",
+                    "username", "john_doe",
+                    "trackingNumber", trackingNumber
+                ))));
+        }
+
+        @Nested
+        @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+        @DisplayName("When PUT /shipments/:id/ship is received over HTTP")
+        class ShipOrder {
+
+            @Test
+            @Order(1)
+            @DisplayName("Then 'order-shipped' is published to kafka")
+            void shouldPublishOrderShipped() {
+                assumeThat(trackingNumber).isNotNull();
+
+                sendPutRequest("/shipments/%s/ship".formatted(trackingNumber),
+                    Map.of("shippedAt", "2025-09-22T11:47:24.000Z", "estimatedDeliveryDate", "2025-09-22T11:47:24.000Z"));
+
+                await().untilAsserted((() -> then(outgoingKafkaMessages.messagesFor("order-shipped"))
+                    .hasSize(1).first().asInstanceOf(MAP)
+                    .containsAllEntriesOf(Map.of(
+                        "orderId", "order-100",
+                        "username", "john_doe",
+                        "trackingNumber", trackingNumber
+                    ))));
+            }
+
+            @Nested
+            @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+            @DisplayName("When PUT /shipments/:id/deliver is received over HTTP")
+            class DeliverOrder {
+
+                @Test
+                @Order(1)
+                @DisplayName("Then 'order-delivered' is published to kafka")
+                void shouldPublishOrderDelivered() {
+                    assumeThat(trackingNumber).isNotNull();
+
+                    sendPutRequest("/shipments/%s/deliver".formatted(trackingNumber), Map.of("deliveredAt", "2025-09-22T11:47:24.000Z"));
+
+                    await().untilAsserted((() -> then(outgoingKafkaMessages.messagesFor("order-delivered"))
+                        .hasSize(1).first().asInstanceOf(MAP)
+                        .containsAllEntriesOf(Map.of(
+                            "orderId", "order-100",
+                            "username", "john_doe",
+                            "trackingNumber", trackingNumber
+                        ))));
+                }
+            }
+        }
     }
 
-    @Test
-    @Order(3)
-    void shouldPublishOrderShipped() {
-        assumeThat(trackingNumber).isNotNull();
-
-        sendPutRequest("/shipments/%s/ship".formatted(trackingNumber), Map.of(
-            "shippedAt", "2025-09-22T11:47:24.000Z",
-            "estimatedDeliveryDate", "2025-09-22T11:47:24.000Z"
-        ));
-
-        await().untilAsserted((() ->
-            then(outgoingKafkaMessages.messagesFor("order-shipped"))
-                .hasSize(1).first()
-                .hasFieldOrPropertyWithValue("orderId", "order-100")
-                .hasFieldOrPropertyWithValue("username", "john_doe")
-                .hasFieldOrPropertyWithValue("trackingNumber", trackingNumber)));
-    }
-
-
-    @Test
-    @Order(4)
-    void shouldPublishOrderDelivered() {
-        assumeThat(trackingNumber).isNotNull();
-
-        sendPutRequest("/shipments/%s/deliver".formatted(trackingNumber), Map.of(
-            "deliveredAt", "2025-09-22T11:47:24.000Z"
-        ));
-
-        await().untilAsserted((() ->
-            then(outgoingKafkaMessages.messagesFor("order-delivered"))
-                .hasSize(1).first()
-                .hasFieldOrPropertyWithValue("orderId", "order-100")
-                .hasFieldOrPropertyWithValue("username", "john_doe")
-                .hasFieldOrPropertyWithValue("trackingNumber", trackingNumber)));
-    }
 }
